d9315325d98c1f7c0de840ed995e2b91baf874f1
stanlylife <stianhave@hotmail.com>
2019-10-20 19:21:37 +0200

    added death transition
    on death there is not a canvas fade in that says "DEAD"

--- MONOBEHAVIOUR/GameSettings.cs
+++ MONOBEHAVIOUR/GameSettings.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameSettings : MonoBehaviour
-{
+public class GameSettings : MonoBehaviour {
 	[Header("WaveSpawner")]
 	public WaveSpawner wave;
-	[Tooltip ("game settings own attribute, is spawning done, forces wave")]public bool gsWaveDone = false;
+	[Tooltip("game settings own attribute, is spawning done, forces wave")] public bool gsWaveDone = false;
 	[Header("Sound settings")]
 	public float masterLevel;
 	public float soundLevel;
 	public float musicLevel;
 
 	[Header("Game Settings")]
-	[Tooltip ("1 by default")]
+	[Tooltip("1 by default")]
 	public float gameSpeed = 1;
-	[Tooltip ("0 by defaut, for easy")]
+	[Tooltip("0 by defaut, for easy")]
 	public int difficulty = 0; //0 easy, 1 medium, 2 hard, 3 impossible
 	[Tooltip("Is game paused, true false")]
 	public bool isPaused = false;
@@ -28,17 +28,20 @@ public class GameSettings : MonoBehaviour
 
 
 	[Header("NextWave button")]
-	[SerializeField]
-	private GameObject button;
-
+	public Button StartButton;
 	[HideInInspector]
 	public bool[] doneSpawning;
 	private bool allDoneSpawning;
 	[HideInInspector]
 	public int currentWave = 0;
+	
+
+	[Header("Death transition panel")]
+	public DeadTransition deadScreen;
+	
 	private void FixedUpdate() {
 		if (waveStart) {
-
+			StartButton.interactable = false;
 			foreach(bool spawner in doneSpawning) {
 				if (spawner) {
 					allDoneSpawning = true;
@@ -62,6 +65,7 @@ public class GameSettings : MonoBehaviour
 			waveStart = false;
 			isPaused = true;
 			gameSpeed = 1;
+			StartButton.interactable = true;
 			//Activate button
 		}
 
@@ -97,5 +101,10 @@ public class GameSettings : MonoBehaviour
 
 	public void takeDamage(int damage) {
 		gameHealth -= damage;
+		if (gameHealth <= 0) {
+			isPaused = true;
+			gameSpeed = 0;
+			deadScreen.PlayAnimation();
+		}
 	}
 }
---
+++ MONOBEHAVIOUR/UI/MainMenuBack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuBack : MonoBehaviour
+{
+	private bool isHTPCanvas;
+	private bool isAboutCanvas;
+
+	public GameObject aboutCanvas;
+	public GameObject HTPCanvas;
+	
+	public void setCanvas(string type) {
+		if(type.ToLower() == "about") {
+			isHTPCanvas = false;
+			isAboutCanvas = true;
+		}else if (type.ToLower() == "htp") {
+			isAboutCanvas = false;
+			isHTPCanvas = true;
+		}
+		openCanvas();
+	}
+
+	public void back() {
+		aboutCanvas.SetActive(false);
+		HTPCanvas.SetActive(false);
+	}
+
+	public void openCanvas() {
+		if (isHTPCanvas) {
+			back();
+			HTPCanvas.SetActive(true);
+		} else if(isAboutCanvas) {
+			back();
+			aboutCanvas.SetActive(true);
+		}
+	}
+}
